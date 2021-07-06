1c0e7206f90772d63d8d56222d6152d7b109ed0c
Juan Galvis <43938697+JuanesGalvis@users.noreply.github.com>
2021-07-05 19:18:21 -0500

    Animacion y servicios casi terminados (falta información para la tarjeta) + Imágenes de mayor tamaño + Alt en una imagen faltante + Actualización de comandos

--- src/index.js
+++ src/index.js
@@ -10,6 +10,12 @@ console.log("COMPILADO TAMBIÉN CORRECTAMENTE!");
 // ########:: ##:::: ##:. ######:: ##::. ##:. ######::: ##:::. ##:. #######::. #######:: ##::. ##: ########::. ######::
 //........:::..:::::..:::......:::..::::..:::......::::..:::::..:::.......::::.......:::..::::..::........::::......:::
 
+
+import AKTMotos from './assets/Brands/Akt-motos.png';
+import RoyalEnfield from './assets/Brands/Royal-Enfield.png';
+import UMA from './assets/Brands/Grupo-UMA.png';
+
+/** IMÁGENES PARA MOBILE */
 import VentaDeRepuestos from './assets/Backgrounds/VentaDeRepuestos.png';
 import Mantenimiento from './assets/Backgrounds/Mantenimiento.png';
 import PuestaPunto from './assets/Backgrounds/PuestaPunto.png';
@@ -17,6 +23,10 @@ import Lavadero from './assets/Backgrounds/Lavadero.png';
 import Revisiones from './assets/Backgrounds/Revisiones.png';
 import Pintura from './assets/Backgrounds/Pintura.png';
 
-import AKTMotos from './assets/Brands/Akt-motos.png';
-import RoyalEnfield from './assets/Brands/Royal-Enfield.png';
-import UMA from './assets/Brands/Grupo-UMA.png';
+/** IMÁGENES PARA TAMAÑO MAYOR */
+import VentaDeRepuestosG from './assets/Backgrounds/VentaDeRepuestosG.png';
+import MantenimientoG from './assets/Backgrounds/MantenimientoG.png';
+import PuestaPuntoG from './assets/Backgrounds/PuestaPuntoG.png';
+import LavaderoG from './assets/Backgrounds/LavaderoG.png';
+import RevisionesG from './assets/Backgrounds/RevisionesG.png';
+import PinturaG from './assets/Backgrounds/PinturaG.png';
--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,6 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
-    mode: 'development',
     module: {
         rules: [
             {
