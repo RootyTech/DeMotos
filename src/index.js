6183ade93f68b6bd674af5b1409a18c231cd9cfd
MiguelABoni <miguelbedoyabonilla@gmail.com>
2021-07-09 13:06:11 -0500

    Animación componente de Marcas Lista!

--- src/index.js
+++ src/index.js
@@ -51,3 +51,28 @@ if (document.querySelector('.carousel__elements')) {
         });    
     });
 }
+
+/* CÓDIGO ANIMACIÓN INDEX -> MARCAS */
+const brands_img = document.querySelectorAll('.Brands__img');
+const windowSize = (window.innerHeight/2);
+let brands_positions=[];
+
+window.addEventListener('scroll', ()=>{
+    brands_positions[0]=brands_img[1].getBoundingClientRect().bottom;
+    brands_positions[1]=brands_img[1].getBoundingClientRect().top;
+
+    if(windowSize > brands_positions[1]){
+        brands_img[0].classList.add('FadeColor');
+        brands_img[1].classList.add('FadeColor');
+        brands_img[2].classList.add('FadeColor');
+    }else{
+        brands_img[0].classList.remove('FadeColor');
+        brands_img[1].classList.remove('FadeColor');
+        brands_img[2].classList.remove('FadeColor');
+    }
+    if(windowSize/2 > brands_positions[0]){
+        brands_img[0].classList.remove('FadeColor');
+        brands_img[1].classList.remove('FadeColor');
+        brands_img[2].classList.remove('FadeColor');
+    }
+})
