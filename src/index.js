2df88c6bb7c7e1fc5506f900ea0c2832562d4972
Santiago Restrepo <santirstrp@gmail.com>
2021-07-27 17:29:52 -0500

    Primera version de menu mobile

--- src/index.js
+++ src/index.js
@@ -60,26 +60,5 @@ Carrousel();
 import {EventListener} from './scripts/Marcas';
 EventListener();
 
-//import {menuEvent} from './scripts/menu';
-//menuEvent();
-
-const menu_container = document.querySelector(".menu__container");
-const menu_button = document.querySelector(".menu__button > button");
-const menu = document.querySelector(".menu");
-console.log(menu_button);
-menu_button.addEventListener("click", function(){
-    console.log("Está entrando");
-    menu_button.classList.toggle("close__button");
-    if (menu__button.classlist.contains("close__button")) {                    
-        menu_container.style.flexDirection = "column-reverse";
-        menu_container.style.justifyContent = "flex-start";
-        menu.style.display = "flex"
-    } else {
-        menu_container.style.flexDirection = "column";
-        menu_container.style.justifyContent = "flex-end";
-        menu.style.display = "none"
-
-    }
-});
-
-
+import {menuEvent} from './scripts/menu';
+menuEvent();
--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,22 +1,15 @@
 export function menuEvent(){
     if (document.querySelector(".menu__container")) {
         const menu_container = document.querySelector(".menu__container");
-        const menu_button = document.querySelector(".menu__button > button");
+        const menu_button = document.querySelector(".menu__button");
         const menu = document.querySelector(".menu");
-        console.log(menu_button);
-        menu_container.addEventListener("click", function(){
-            console.log("Está entrando");
-            menu_button.classList.toggle("close__button");
-            if (menu__button.classlist.contains("close__button")) {                    
-                menu_container.style.flexDirection = "column-reverse";
-                menu_container.style.justifyContent = "flex-start";
-                menu.style.display = "flex"
-            } else {
-                menu_container.style.flexDirection = "column";
-                menu_container.style.justifyContent = "flex-end";
-                menu.style.display = "none"
+        
 
-            }
+        menu_button.addEventListener("click", function(){
+            menu_button.classList.toggle("close__button");
+            menu_container.classList.toggle("is-active");
+            menu.classList.toggle("is-active");
+            menu.style.transition = "transform 1s";
         });
     }
 }
