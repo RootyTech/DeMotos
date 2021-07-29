259d56f982b6c0e7e74796b33c36a23eaed39088
Santiago Restrepo <santirstrp@gmail.com>
2021-07-28 19:30:56 -0500

    Termiando el menu mobile sin bugs

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -2,14 +2,40 @@ export function menuEvent(){
     if (document.querySelector(".menu__container")) {
         const menu_container = document.querySelector(".menu__container");
         const menu_button = document.querySelector(".menu__button");
-        const menu = document.querySelector(".menu");
+        const modal = document.querySelector(".modal");
+        const bodyDocument = document.querySelector("body");
+
         
 
-        menu_button.addEventListener("click", function(){
-            menu_button.classList.toggle("close__button");
+        const activateMenu = ()=>{
+            menu_button.classList.toggle("close-button");
             menu_container.classList.toggle("is-active");
-            menu.classList.toggle("is-active");
-            menu.style.transition = "transform .7s";
-        });
+            modal.classList.toggle("modal-active");
+            if(menu_container.classList.contains("is-active")){
+                bodyDocument.style.overflowY = "hidden"
+            }else{
+                bodyDocument.style.overflowY = "scroll"
+            }    
+            if(menu_container.style.transitionDuration === ""){
+                menu_container.style.transitionDuration = "1s";
+            }
+        }
+
+        const closeMenuOutside = (e)=>{
+            if(e.target != menu_button && e.target != menu_button.firstElementChild){
+                menu_button.classList.toggle("close-button");
+                menu_container.classList.toggle("is-active");
+                modal.classList.toggle("modal-active");
+                if(menu_container.classList.contains("is-active")){
+                    bodyDocument.style.overflowY = "hidden";
+                }else{
+                    bodyDocument.style.overflowY = "scroll";
+                }
+            }
+        }
+
+        menu_button.addEventListener("click", activateMenu);
+        menu_container.addEventListener("click", closeMenuOutside);
+
     }
 }
