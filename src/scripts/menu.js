4e14b6e11cd8e7b4cc717f062d015b9e865443a7
Santiago Restrepo <santirstrp@gmail.com>
2021-10-06 17:49:59 -0500

    correción de errores con zindez del logo

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,4 +1,5 @@
 export function menuEvent(){
+        const logo = document.querySelector(".home__link");
         const menu_container = document.querySelector(".menu__container");
         const menu_button = document.querySelector(".menu__button");
         const menu = document.querySelector(".menu");
@@ -10,25 +11,28 @@ export function menuEvent(){
             
             const activateMenu = ()=>{
                 //Una vez se active el menú se añade el evento para cerrarlo
-                
+                logo.classList.toggle("over");
                 menu_button.classList.toggle("close-button");
                 menu_container.classList.toggle("is-active");
                 modal.classList.toggle("modal-active");
+                //Este condicional es para saber si el menú fue abierto o cerrado a través del botón hamburguesa
                 if(menu_container.classList.contains("is-active")){
+                    menu_container.addEventListener("click", closeMenuOutside);
+                    modal.addEventListener("click", closeMenuOutside);
                     bodyDocument.style.overflowY = "hidden"
                 }else{
                     bodyDocument.style.overflowY = "scroll"
+                    menu_container.removeEventListener("click", closeMenuOutside);
                 }    
                 if(menu_container.style.transitionDuration === ""){
                     menu_container.style.transitionDuration = "1s";
                 }
-                menu_container.addEventListener("click", closeMenuOutside);
-                modal.addEventListener("click", closeMenuOutside);
             }
             
             const closeMenuOutside = (e)=>{
                 //Se comprueba que el target sea diferente al botón y a la barra para que no se aplique el toggle 2 veces al mismo tiempo
                 if(e.target != menu_button && e.target != menu_button.firstElementChild && e.target != menu){
+                    logo.classList.toggle("over");
                     menu_button.classList.remove("close-button");
                     menu_container.classList.remove("is-active");
                     modal.classList.remove("modal-active");
@@ -37,6 +41,7 @@ export function menuEvent(){
                     }else{
                         bodyDocument.style.overflowY = "scroll";
                     }
+                    menu_container.removeEventListener("click", closeMenuOutside);
                 }
             }
     
